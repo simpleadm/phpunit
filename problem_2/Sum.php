e5d7638f800ec3af86653cd299c2dc2028ca15fb
simpleadm <spetr@softserveinc.com>
2013-03-20 16:06:39 +0200

    Format fix

--- problem_2/Sum.php
+++ problem_2/Sum.php
@@ -2,40 +2,41 @@
 
 class Sum {
 
-	public function fib($num = 3) {
-        $fibs = array(1,2);
+    public function fib($num = 3) {
+        $fibs = array(1, 2);
 
-        if ($num < 3) { 
-        	$num = 3; 
+        if ($num < 3) {
+            $num = 3;
         }
-        
+
         for ($i = 2; $i < $num; $i++) {
-            $fibs[$i] = $fibs[$i - 1] + $fibs[$i - 2]; 
+            $fibs[$i] = $fibs[$i - 1] + $fibs[$i - 2];
         }
 
         return $fibs;
     }
 
     public function getEvens($nums) {
-    	$filtered = array_filter($nums, function ($num){
-    		if( $num%2 == 0 ) return $num;
-    	});
+        $filtered = array_filter($nums, function ($num) {
+                    if ($num % 2 == 0)
+                        return $num;
+                });
 
-    	return array_values($filtered);
+        return array_values($filtered);
     }
 
     public function fibSum($max) {
-    	$num = 3;
-    	$fibs = array();
+        $num = 3;
+        $fibs = array();
 
-    	do {
-    		$fibs = $this->fib($num);
-    		$num++;
-    	} while( array_pop($fibs) <= $max );
+        do {
+            $fibs = $this->fib($num);
+            $num++;
+        } while (array_pop($fibs) <= $max);
 
-    	$sum = array_sum( $this->getEvens($fibs) );
+        $sum = array_sum($this->getEvens($fibs));
 
-    	return $sum;
+        return $sum;
     }
 
 }
--- problem_2/SumTest.php
+++ problem_2/SumTest.php
@@ -4,69 +4,68 @@ include 'Sum.php';
 
 class SumTest extends PHPUnit_Framework_TestCase {
 
-	public function setUp() {
-		$this->assertTrue(class_exists('Sum'), 'Sum class not exists');
-		
-		$this->sum = new Sum;
-		$this->assertTrue($this->sum instanceof Sum, 'Not a Sum object');
-	}
+    public function setUp() {
+        $this->assertTrue(class_exists('Sum'), 'Sum class not exists');
 
-	/**
-	 *	@dataProvider fibsDataProvider
-	 **/
-	public function testFib($num, $result) {
-		$this->assertTrue(method_exists($this->sum, 'fib'), 'fib() method not exists');
+        $this->sum = new Sum;
+        $this->assertTrue($this->sum instanceof Sum, 'Not a Sum object');
+    }
 
-		$this->assertEquals($this->sum->fib($num), $result, 'fib() bad result');
-	}
+    /**
+     * 	@dataProvider fibsDataProvider
+     * */
+    public function testFib($num, $result) {
+        $this->assertTrue(method_exists($this->sum, 'fib'), 'fib() method not exists');
 
-	public function fibsDataProvider() {
+        $this->assertEquals($this->sum->fib($num), $result, 'fib() bad result');
+    }
+
+    public function fibsDataProvider() {
 
         return array(
-        	array(3, array(1,2,3) ),
-            array(4,	array(1,2,3,5) ),
-            array(6,	array(1,2,3,5,8,13) ),
-            array(7,	array(1,2,3,5,8,13,21) ),
-            array(10,	array(1,2,3,5,8,13,21,34,55,89) ),
+            array(3, array(1, 2, 3)),
+            array(4, array(1, 2, 3, 5)),
+            array(6, array(1, 2, 3, 5, 8, 13)),
+            array(7, array(1, 2, 3, 5, 8, 13, 21)),
+            array(10, array(1, 2, 3, 5, 8, 13, 21, 34, 55, 89)),
         );
     }
 
     /**
      * @dataProvider getEvensDataProvider
-     */ 
+     */
     public function testGetEvens($input, $result) {
-    	$this->assertTrue(method_exists($this->sum, 'getEvens'), 'getEvens() method not exists');
+        $this->assertTrue(method_exists($this->sum, 'getEvens'), 'getEvens() method not exists');
 
-    	$this->assertEquals($this->sum->getEvens($input), $result, 'getEvens() bad result');
+        $this->assertEquals($this->sum->getEvens($input), $result, 'getEvens() bad result');
     }
 
     public function getEvensDataProvider() {
-    	return array(
-        	array(array(1), array()),
-            array(array(1,2), array(2)),
-            array(array(1,2,3), array(2)),
-            array(array(1,2,3,4,5,6,7,8,9,10), array(2,4,6,8,10)),
+        return array(
+            array(array(1), array()),
+            array(array(1, 2), array(2)),
+            array(array(1, 2, 3), array(2)),
+            array(array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), array(2, 4, 6, 8, 10)),
         );
     }
 
     /**
      * @dataProvider fibsSumDataProvider
-     **/ 
+     * */
     public function testFibSum($max, $result) {
-    	$this->assertTrue(method_exists($this->sum, 'fibSum'), 'fibSum() method not exists');
+        $this->assertTrue(method_exists($this->sum, 'fibSum'), 'fibSum() method not exists');
 
-    	$this->assertEquals($this->sum->fibSum($max), $result, 'fibSum() bad result');
+        $this->assertEquals($this->sum->fibSum($max), $result, 'fibSum() bad result');
     }
 
     public function fibsSumDataProvider() {
-    	return array(
-        	array(3, 	2),
-            array(5, 	2),
-            array(13, 	10),
-            array(21,	10),
-            array(89,	44),
+        return array(
+            array(3, 2),
+            array(5, 2),
+            array(13, 10),
+            array(21, 10),
+            array(89, 44),
         );
     }
-
 
 }
